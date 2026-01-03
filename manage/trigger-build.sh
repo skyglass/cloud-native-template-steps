8ac8176d3cf001b4993c8e9c56edf6c4a17116b1
skyglass <skyglass2001@gmail.com>
2026-01-03 17:04:19 +0100

    Create AWS EKS cluster with Terraform

--- manage/trigger-build.sh
+++ manage/trigger-build.sh
@@ -1,8 +1,12 @@
 #! /bin/bash -e
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
 # use sed to replace DUMMY_.* with DUMP_<current timestamp> in application/customer-service/customer-service-deployment/helm-charts/customer-service/values.yaml
 
 FILE=application/customer-service/customer-service-deployment/helm-charts/customer-service/values.yaml
+
+cd $DIR/..
 
 sed -i '' -e "s/DUMMY_.*$/DUMMY_$(date +%s)/" $FILE
 
