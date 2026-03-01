8377475bfaba0687fd0b3b3814ac1f569dd36e0d
skyglass <skyglass2001@gmail.com>
2026-03-02 00:49:04 +0100

    - try to fix the issue with eventuate cdc and other applications

--- terraform-eks/variables.tf
+++ terraform-eks/variables.tf
@@ -3,5 +3,5 @@
 variable "ingress_cidrs" {
     description = "List of CIDR blocks that can access the EKS cluster and Ingress"
     type    = list(string)
-    default = ["94.139.23.78/32"]
+    default = ["94.139.28.51/32"]
 }
